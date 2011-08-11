c1847d06de1de646e6d0a108cf8c3f4e2210600e
gaurav kapoor <mail.gauravkapoor@gmail.com>
2011-08-11 03:59:28 -0400

    class based views for listing and detail pages

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from voting.views import vote_on_object
 from institute.models import Institute
 from event.models import Event
 from article.models import Article
-import views
+from school.models import School
+from core.views import GenericList,GenericDetail
 
 institute_dict = {
     'model': Institute,
@@ -22,8 +23,10 @@ event_dict = {
     'template_object_name': 'event',
 }
 
-
-
+school_dict = {
+    'model': School,
+    'template_object_name': 'school',
+}
 
 urlpatterns = patterns('',
         url(r'^admin/', include(admin.site.urls),name="core_admin"),
@@ -34,21 +37,23 @@ urlpatterns = patterns('',
         
         url(r'^refine_data/$', 'core.views.refine_data',name='refine_data'),
         
-        url(r'^listing/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/(?P<category>[-\w]+)/$', 'core.views.generic_list', name='generic_list'),
+        url(r'^listing/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/(?P<category>[-\w]+)/$',GenericList, name='generic_list'),
         
         url(r'^popular/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/$', 'core.views.data_by_voting_list', name='data_by_voting_list'),
         
-        url(r'^listing/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/(?P<category>[-\w]+)/(?P<state>[-\w]+)/$', 'core.views.generic_list', name='generic_list'),
+        url(r'^listing/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/(?P<category>[-\w]+)/(?P<state>[-\w]+)/$',GenericList, name='generic_list'),
         
-        url(r'^listing/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/$', 'core.views.generic_list', name='generic_list'),
+        url(r'^listing/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/$',GenericList, name='generic_list'),
         
-        url(r'^detail/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/(?P<instance_slug>[-\w]+)/$', 'core.views.generic_detail', name='generic_detail'),
+        url(r'^detail/(?P<app>[-\w]+)/(?P<model_name>[-\w]+)/(?P<instance_slug>[-\w]+)/$',GenericDetail, name='generic_detail'),
         
         url(r'^institute/Institute/(?P<object_id>.+)/(?P<direction>up|down|clear)/?$', vote_on_object, institute_dict, name='institute_vote'),
                 
         url(r'^article/Article/(?P<object_id>.+)/(?P<direction>up|down|clear)/?$', vote_on_object, article_dict, name='article_vote'),
         
         url(r'^event/Event/(?P<object_id>.+)/(?P<direction>up|down|clear)/?$', vote_on_object, event_dict, name='event_vote'),
+        
+         url(r'^school/School/(?P<object_id>.+)/(?P<direction>up|down|clear)/?$', vote_on_object, school_dict, name='school_vote'),
         
         url(r'^contact_us/$', 'core.views.contact_us',name='contactus'),
         
